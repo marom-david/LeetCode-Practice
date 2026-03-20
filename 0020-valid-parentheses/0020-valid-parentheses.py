d81c45004b28b959a9df58e411d5f6212744eb74
Marom David <marom050@gmail.com>
2026-03-20 18:34:06 +0200

    Time: 3 ms (75.04%), Space: 12.5 MB (87.55%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')' : '(',
+            '}' : '{',
+            ']' : '['}
+
+        for c in s:
+            if c in mapping:
+                if not stack or stack.pop() != mapping[c]:
+                    return False
+            else:
+                stack.append(c)
+            
+        
+        return not stack
