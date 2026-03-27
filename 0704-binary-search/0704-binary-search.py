25b657bbee30c55c1a330ee53e7528936114b6b7
Marom David <marom050@gmail.com>
2026-03-27 16:09:07 +0300

    Time: 9 ms (5.3%), Space: 13.2 MB (85.25%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -2,7 +2,6 @@ class Solution(object):
     def search(self, nums, target):
         l = 0
         r = len(nums) - 1
-        index = (l+r)//2
 
         while l <= r:
             index = (l+r)//2
