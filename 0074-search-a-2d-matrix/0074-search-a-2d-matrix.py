4971dc8f1cbb131d51e47ea29762823dca65e53a
Marom David <marom050@gmail.com>
2026-03-28 13:24:51 +0300

    Time: 0 ms (100%), Space: 12.5 MB (98.05%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,16 +3,17 @@ class Solution(object):
         n = len(matrix[0])
         l = 0
         r = len(matrix)*len(matrix[0]) - 1
+        index = (l+r)//2
 
         while l <= r:
             index = (l+r)//2
-            col = index%n
-            row = index//n 
-            if matrix[row][col] == target:
+            col = index//n
+            row = index%n
+            if matrix[col][row] == target:
                 return True
-            elif matrix[row][col] < target:
-                l = index + 1
-            elif matrix[row][col] > target:
+            if matrix[col][row] > target:
                 r = index - 1
-        
+            if matrix[col][row] < target:
+                l = index + 1
+
         return False
